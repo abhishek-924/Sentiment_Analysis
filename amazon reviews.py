f1d6212bf72e57eb2e217e96d90744f6e6c133d4
abhishek-924 <38368943+abhishek-924@users.noreply.github.com>
2018-10-02 17:48:10 +0530

    Update amazon reviews.py

--- amazon reviews.py
+++ amazon reviews.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 df=pd.read_csv('C:\ghost drive\study\ds\sentiment analysis\Amazon_Unlocked_Mobile.csv')
 df.dropna(inplace=True) 
 df=df[df['Rating']!=3] 
